c468647142fe423e5267b60fdfa430a49d603fec
Arturo Valdez <santiguardado27@gmail.com>
2025-02-04 20:32:34 -0600

    Inicio de proyecto Maquina de Snacks

---
+++ src/maquina_snack/Snack.java
@@ -0,0 +1,22 @@
+package maquina_snack;
+import java.io.Serializable;
+
+//Uso de javabeans, omponentes reutilizables en Java que se
+//utilizan para encapsular datos y comportamientos en aplicaciones
+public class Snack implements Serializable {
+    //Atributos
+    private static int contadorSnacks =0;
+    private int idSnack;
+    private String nombre;
+    private double precio;
+
+    public Snack(){
+        this.idSnack = ++Snack.contadorSnacks;
+    }
+
+    public Snack(String nombre, double precio){
+        this();//manda a llamar constructor vacio
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
