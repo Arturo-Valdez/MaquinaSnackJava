d4c47ffbda0aabf28d6636b786956d572da95965
Arturo Valdez <santiguardado27@gmail.com>
2025-02-05 17:29:22 -0600

    Creacion de clase Snack

--- src/maquina_snack/Snack.java
+++ src/maquina_snack/Snack.java
@@ -1,5 +1,6 @@
 package maquina_snack;
 import java.io.Serializable;
+import java.util.Objects;
 
 //Uso de javabeans, componentes reutilizables en Java que se
 //utilizan para encapsular datos y comportamientos en aplicaciones
@@ -10,6 +11,7 @@ public class Snack implements Serializable {
     private String nombre;
     private double precio;
 
+    //Constructor vacio por JavaBeans
     public Snack(){
         this.idSnack = ++Snack.contadorSnacks;
     }
@@ -18,5 +20,50 @@ public class Snack implements Serializable {
         this();//manda a llamar constructor vacio
         this.nombre = nombre;
         this.precio = precio;
+    }
+
+    public int getIdSnack() {
+        return idSnack;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public static int getContadorSnacks() {
+        return contadorSnacks;
+    }
+
+    @Override
+    public String toString() {
+        return "Snack{" +
+                "idSnack=" + idSnack +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Snack snack = (Snack) o;
+        return idSnack == snack.idSnack && Double.compare(precio, snack.precio) == 0 && Objects.equals(nombre, snack.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idSnack, nombre, precio);
     }
 }
